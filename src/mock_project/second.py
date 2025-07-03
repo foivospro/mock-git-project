ebbe0915d31851112e480625b61dfc7b36f01fbb
foiv <t8210126@aueb.gr>
2025-07-03 19:11:38 +0300

    add file

---
+++ src/mock_project/second.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import django
+import fastapi
+import sqlalchemy
+
+def process_data_second():
+    data = [1, 2, 3, 4, 5]
+    result = sum(data)
+    return result
+
+def calculate_average_second():
+    numbers = [10, 20, 30, 40, 50]
+    average = sum(numbers) / len(numbers)
+    return average
